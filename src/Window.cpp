a39c33190c46ed7165c82d0c7af91331a7fbe8c0
Anjolaoluwa Olubusi (Windows) <anjolaolubusi@gmail.com>
2020-11-08 21:54:12 -0600

    Added some documentation to the Windows class and restructed the Window class

--- src/Window.cpp
+++ src/Window.cpp
@@ -2,52 +2,76 @@
 #include <iostream>
 
 
+//Parameterized constructor
 Window::Window(int width, int height, std::string title)
 {
+    //Initalizes glfw
     if(!glfwInit()){
         std::cout << "Failed to initalize GLFW" << std::endl;
     }
+    
+    //Defines how errors are handled
     glfwSetErrorCallback(ErrorCallback);
+    
+    //Sets up Window class
     window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
+    
+    //Check if window is probably defined
     if(!this->window)
     {
         std::cout << "Failed to initalize window" << std::endl;
+        glfwTerminate();
     }
+    
+    //Defines OpenGL context
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    //Sets current context to this window
     glfwMakeContextCurrent(this->window);
+    
+    //Imports glad
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
     }
-    glfwSetKeyCallback(window, key_callback);
+    
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
+    glfwSetFramebufferSizeCallback(window, Resize);
 }
 
+//Checks if window is running
 bool Window::isRunning(){
     return !glfwWindowShouldClose(this->window);
 }
 
-void Window::CloseWindow(){
+//Closes all glfw process
+void Window::CloseAllGLFW(){
     glfwTerminate();
     std::cout << "GLFW terminated" << std::endl;
 
 }
 
+//Error handeller
 void Window::ErrorCallback(int error, const char* description){
     fprintf(stderr, "Error: %s\n", description);
 }
 
-void Window::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
 
+//Updates screen
 void Window::Update(){
     glfwSwapBuffers(this->window);
     glfwSwapInterval(1);
     glfwPollEvents();
+}
+
+void Window::Resize(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
 
 Window::~Window()
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include "Window.h"
 
+void Input(GLFWwindow* window, int key, int scancode, int action, int mods){
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
 int main()
 {
 Window screen(800, 600, "Test");
+glfwSetKeyCallback(screen.window, Input);
 while(screen.isRunning()){
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
     screen.Update();
 }
-screen.CloseWindow();
+screen.CloseAllGLFW();
 
 }
 
